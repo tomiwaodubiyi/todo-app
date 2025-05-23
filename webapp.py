4271d37819559eb46dcfc9c1ffe160fa070465d3
tomiwaodubiyi <odubiyitomiwa@gmail.com>
2025-05-23 15:48:57 -0700

    Implemented the "Add todo" feature using textbox.

--- webapp.py
+++ webapp.py
@@ -3,6 +3,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo_input = st.session_state["new_todo"] + '\n'
+    todos.append(todo_input)
+    functions.write_todos(todos)
+
+
 st.title("To-do App")
 #st.subheader("This is my to-do app")
 st.text("Boost your productivity by keeping track of your todos")
@@ -11,5 +18,5 @@ st.text("Boost your productivity by keeping track of your todos")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter a new todo here..")
-
+st.text_input(label="", placeholder="Enter a new todo here..",
+              on_change=add_todo, key="new_todo")
